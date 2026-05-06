847b0c131462204fd418059439202ead82e321fd
alejay01 <alejay@gmail.com>
2026-05-06 07:31:55 -0500

    Add safe MySQL config and status checks

--- public_html/test/rewards/_includes/bootstrap.php
+++ public_html/test/rewards/_includes/bootstrap.php
@@ -3,6 +3,8 @@ declare(strict_types=1);
 
 date_default_timezone_set('America/Chicago');
 
+require_once __DIR__ . '/database.php';
+
 $app = [
     'name' => 'The Boudin Company Rewards',
     'restaurant' => 'The Boudin Company',
